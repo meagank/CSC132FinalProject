fefdbd5c5ea3889102e96cde3799002dc2d4f3c3
Jakira Williams <jalanaw16@gmail.com>
2020-04-21 18:25:00 -0500

    added checker pieces

--- checkers.py
+++ checkers.py
@@ -99,7 +99,57 @@ class Board(Frame):
         canvas.create_rectangle(674, 374, 766, 424, outline="black", fill="black")
 
         canvas.pack(fill=BOTH, expand=1)
-    
+
+
+    ##### RED CHECKER PIECES #####
+        
+        #### ROW 1 ####
+        r1 = canvas.create_oval(30, 10, 122, 62, outline="red", fill="red")
+        r2 = canvas.create_oval(122, 10, 214, 62, outline="red", fill="red")
+        r3 = canvas.create_oval(214, 10, 306, 62, outline="red", fill="red")
+        r4 = canvas.create_oval(306, 10, 398, 62, outline="red", fill="red")
+        
+        
+
+        #### ROW 2 ####
+        r5 = canvas.create_oval(30, 62, 122, 114, outline="red", fill="red")
+        r6 = canvas.create_oval(122, 62, 214, 114, outline="red", fill="red")
+        r7 = canvas.create_oval(214, 62, 306, 114, outline="red", fill="red")
+        r8 = canvas.create_oval(306, 62, 398, 114, outline="red", fill="red")
+        
+
+
+        #### ROW 3 ####
+        r9 = canvas.create_oval(30, 114, 122, 166, outline="red", fill="red")
+        r10 = canvas.create_oval(122, 114, 214, 166, outline="red", fill="red")
+        r11 = canvas.create_oval(214, 114, 306, 166, outline="red", fill="red")
+        r12 = canvas.create_oval(306, 114, 398, 166, outline="red", fill="red")
+
+
+    ##### BLUE CHECKER PIECES #####
+        
+        #### ROW 6 ####
+        b1 = canvas.create_oval(30, 270, 122, 322, outline="blue", fill="blue")
+        b2 = canvas.create_oval(122, 270, 214, 322, outline="blue", fill="blue")
+        b3 = canvas.create_oval(214, 270, 306, 322, outline="blue", fill="blue")
+        b4 = canvas.create_oval(306, 270, 398, 322, outline="blue", fill="blue")
+        
+        
+
+        #### ROW 7 ####
+        b5 = canvas.create_oval(30, 322, 122, 374, outline="blue", fill="blue")
+        b6 = canvas.create_oval(122, 322, 214, 374, outline="blue", fill="blue")
+        b7 = canvas.create_oval(214, 322, 306, 374, outline="blue", fill="blue")
+        b8 = canvas.create_oval(306, 322, 398, 374, outline="blue", fill="blue")
+        
+
+
+        #### ROW 8 ####
+        b9 = canvas.create_oval(30, 374, 122, 424, outline="blue", fill="blue")
+        b10 = canvas.create_oval(122, 374, 214, 424, outline="blue", fill="blue")
+        b11 = canvas.create_oval(214, 374, 306, 424, outline="blue", fill="blue")
+        b12 = canvas.create_oval(306, 374, 398, 424, outline="blue", fill="blue")        
+        
 
 def main():
 
