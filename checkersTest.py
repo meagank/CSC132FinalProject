40d5877be4846a5b5bdae0b36863857112752f61
Meagan Kropp <meagan.kropp@gmail.com>
2020-04-21 19:27:07 -0500

    formatting

--- checkersTest.py
+++ checkersTest.py
@@ -103,53 +103,47 @@ class Board(Frame):
 
     ##### RED CHECKER PIECES #####
         
-        #### ROW 1 ####
+        #### COLUMN 1 ####
         r1 = canvas.create_oval(30, 10, 122, 62, outline="red", fill="red")
-        r2 = canvas.create_oval(214, 10, 306, 62, outline="red", fill="red")
-        r3 = canvas.create_oval(398, 10, 490, 62, outline="red", fill="red")
-        r4 = canvas.create_oval(582, 10, 674, 62, outline="red", fill="red")
-        
+        r2 = canvas.create_oval(30, 114, 122, 166, outline="red", fill="red")
+        r3 = canvas.create_oval(30, 218, 122, 270, outline="red", fill="red")
+        r4 = canvas.create_oval(30, 322, 122, 374, outline="red", fill="red")
         
 
-        #### ROW 2 ####
+        #### COLUMN 2 ####
         r5 = canvas.create_oval(122, 62, 214, 114, outline="red", fill="red")
-        r6 = canvas.create_oval(306, 62, 398, 114, outline="red", fill="red")
-        r7 = canvas.create_oval(490, 62, 582, 114, outline="red", fill="red")
-        r8 = canvas.create_oval(674, 62, 766, 114, outline="red", fill="red")
-        
+        r6 = canvas.create_oval(122, 166, 214, 218, outline="red", fill="red")
+        r7 = canvas.create_oval(122, 270, 214, 322, outline="red", fill="red")
+        r8 = canvas.create_oval(122, 374, 214, 424, outline="red", fill="red")
 
-
-        #### ROW 3 ####
-        r9 = canvas.create_oval(30, 114, 122, 166, outline="red", fill="red")
+        #### COLUMN 3 ####
+        r9 = canvas.create_oval(214, 10, 306, 62, outline="red", fill="red")
         r10 = canvas.create_oval(214, 114, 306, 166, outline="red", fill="red")
-        r11 = canvas.create_oval(398, 114, 490, 166, outline="red", fill="red")
-        r12 = canvas.create_oval(582, 114, 674, 166, outline="red", fill="red")
+        r11 = canvas.create_oval(214, 218, 306, 270, outline="red", fill="red")
+        r12 = canvas.create_oval(214, 322, 306, 374, outline="red", fill="red")
 
 
     ##### BLUE CHECKER PIECES #####
-        
-        #### ROW 6 ####
-        b1 = canvas.create_oval(122, 270, 214, 322, outline="blue", fill="blue")
-        b2 = canvas.create_oval(306, 270, 398, 322, outline="blue", fill="blue")
+
+        #### COLUMN 1 ####
+        b1 = canvas.create_oval(490, 62, 582, 114, outline="blue", fill="blue")
+        b2 = canvas.create_oval(490, 166, 582, 218, outline="blue", fill="blue")
         b3 = canvas.create_oval(490, 270, 582, 322, outline="blue", fill="blue")
-        b4 = canvas.create_oval(674, 270, 766, 322, outline="blue", fill="blue")
-        
-        
+        b4 = canvas.create_oval(490, 374, 582, 424, outline="blue", fill="blue")
 
-        #### ROW 7 ####
-        b5 = canvas.create_oval(30, 322, 122, 374, outline="blue", fill="blue")
-        b6 = canvas.create_oval(214, 322, 306, 374, outline="blue", fill="blue")
-        b7 = canvas.create_oval(398, 322, 490, 374, outline="blue", fill="blue")
+        #### COLUMN 2 ####
+        b5 = canvas.create_oval(582, 10, 674, 62, outline="blue", fill="blue")
+        b6 = canvas.create_oval(582, 114, 674, 166, outline="blue", fill="blue")
+        b7 = canvas.create_oval(582, 218, 674, 270, outline="blue", fill="blue")
         b8 = canvas.create_oval(582, 322, 674, 374, outline="blue", fill="blue")
-        
 
 
-        #### ROW 8 ####
-        b9 = canvas.create_oval(122, 374, 214, 424, outline="blue", fill="blue")
-        b10 = canvas.create_oval(306, 374, 398, 424, outline="blue", fill="blue")
-        b11 = canvas.create_oval(490, 374, 582, 424, outline="blue", fill="blue")
+        #### COLUM 3 ####
+        b9 = canvas.create_oval(674, 62, 766, 114, outline="blue", fill="blue")
+        b10 = canvas.create_oval(674, 166, 766, 218, outline="blue", fill="blue")
+        b11 = canvas.create_oval(674, 270, 766, 322, outline="blue", fill="blue")
         b12 = canvas.create_oval(674, 374, 766, 424, outline="blue", fill="blue")
-
+  
 
                
         canvas.pack(fill=BOTH, expand=1)
