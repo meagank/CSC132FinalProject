26d0e5e6d8b16dd24a96c0cf74939bed9591597d
Jakira Williams <jalanaw16@gmail.com>
2020-05-13 18:59:28 -0500

    made checkerboard a background

--- checkersTest2.py
+++ checkersTest2.py
@@ -5,15 +5,18 @@ root = Tk()
 root.title("Checkers")
 canvas = Canvas(root, width = 300 , height = 300)
 canvas.pack()
-img = PhotoImage(file="images/CheckerBoard.gif")
-canvas.create_image(20, 20, anchor = NW, image = img)
-photo = PhotoImage(file="images/Red piece.gif")
-Button(root, image = photo).pack(side = TOP)
-photo1 = PhotoImage(file="images/Black piece.gif")
-Button(root, image = photo1).pack(side = TOP)
-
+background_image = PhotoImage(file="images/CheckerBoard.gif")
+background_label = Label(image = background_image)
+background_label.place(x=0, y=0, relwidth=1,relheight=1)
+##    canvas.create_image(20, 20, anchor = NW, image = img)
+##    photo = PhotoImage(file="images/Red piece.gif")
+##    Button(root, image = photo).pack(side = TOP)
+##    photo1 = PhotoImage(file="images/Black piece.gif")
+##    Button(root, image = photo1).pack(side = TOP)
+##
 
 
 ##window = Tk()
 
 mainloop()
+
